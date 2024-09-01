b29be032e12e2333becb8b3ed25bf13fd53f9a60
Belayne <37399395+Belayne@users.noreply.github.com>
2024-09-01 22:48:50 +0200

    feat: add multiple characters to speechbox

--- src/components/SpeechBox.tsx
+++ src/components/SpeechBox.tsx
@@ -1,50 +1,92 @@
 import "./speechBox.css";
 import { useState, useEffect, useRef} from "react";
 
-const lines: String[] = [
-    `Alright, Morty, pay attention—this game’s as simple as it gets.`,
-    `You see those cards?`,
-    `Your job is to click on ‘em.`,
-    `But only the ones you haven’t clicked before.`
+
+const dialogue = [
+    {
+        name: "Rick",
+        lines: [
+            `Alright, Morty, pay attention—this game’s as simple as it gets.`,
+            `You see those cards?`,
+            `Your job is to click on ‘em.`,
+            `But only the ones you haven’t clicked before.`
+        ],
+        image: "https://pngimg.com/d/rick_morty_PNG28.png"
+    },
+    {
+        name: "Morty",
+        lines: [
+            "Wait, Rick, that’s it? Just click the cards?"
+        ],
+        image: "https://e7.pngegg.com/pngimages/776/370/png-clipart-morty-smith-amazon-com-computer-mouse-mouse-mats-rick-sanchez-skin-electronics-face-thumbnail.png"
+    },
+    {
+        name: "Rick",
+        lines: [
+            `Exactly!`,
+            `Each new card you click boosts your score.`,
+            `But if you click one twice, it’s all over, Morty.`,
+            `No second chances, no retries.`,
+            'Just kaboom—world’s done for.'
+        ],
+        image: "https://pngimg.com/d/rick_morty_PNG28.png"
+    },
+    {
+        name: "Morty",
+        lines: [
+            `Why does it always have to be so stressful, Rick?!`,
+        ],
+        image: "https://e7.pngegg.com/pngimages/776/370/png-clipart-morty-smith-amazon-com-computer-mouse-mouse-mats-rick-sanchez-skin-electronics-face-thumbnail.png"
+    },
+    {
+        name: "Rick",
+        lines: [
+            'IDK YET.'
+        ],
+        image: "https://pngimg.com/d/rick_morty_PNG28.png"
+    },
 ]
 
-
 export default function SpeechBox() {
-    //TODO: probably lift line state to a parent component "DialogueBox" so it can switch beetween characters
-    //pass lines and char image as props
-    //create a character object that contains lines and character image then pass it to DialogueBox
 
-    const [text, setText] = useState<String>("");
-    const [currentChar, setCurrentChar] = useState(0);
-    const [currentLine, setCurrentLine] = useState(0);
-    const speechTime = useRef(25);
+    const [text, setText] = useState<string>("");
+    const [turn, setTurn] = useState<number>(0);
+    const [currentChar, setCurrentChar] = useState<number>(0);
+    const [currentLine, setCurrentLine] = useState<number>(0);
+    const speechTime = useRef<number>(25);
     
-    let lineEnd: Boolean = currentChar === lines[currentLine].length;
+    let lineEnd: boolean = currentChar === dialogue[turn].lines[currentLine].length;
 
-    function setNewText(lineString: String): void {
+    function setNewText(lineString: string): void {
         if(currentChar < lineString.length) {
-            const newText = lineString.slice(0, currentChar + 1);
+            const newText: string = lineString.slice(0, currentChar + 1);
             setText(newText);
             setCurrentChar(currentChar + 1);
         }
     }
 
-    function setNewLine() {
+    function handleText(): void {
         if(!lineEnd) {
             speechTime.current = 0;
             lineEnd = true;
             return;
         }
 
-        if(currentLine < lines.length - 1 && lineEnd) {
+        if(currentLine < dialogue[turn].lines.length - 1 && lineEnd) {
             speechTime.current = 25;
             setCurrentChar(0);
             setCurrentLine(currentLine + 1);
         }
-    }
 
+        if(currentLine === dialogue[turn].lines.length - 1 && lineEnd && turn < dialogue.length - 1) {
+            speechTime.current = 25;
+            setCurrentChar(0);
+            setCurrentLine(0);
+            setTurn(turn + 1);
+        }
+    }
     useEffect(() => {
-        const key = setTimeout(() => {setNewText(lines[currentLine])}, speechTime.current)
+        const key = setTimeout(() => {setNewText(dialogue[turn].lines[currentLine])}, speechTime.current)
         return () => {
             clearTimeout(key)
         }
@@ -52,8 +94,11 @@ export default function SpeechBox() {
 
     return (
         <div className="speechBox">
-            <img src="https://pngimg.com/d/rick_morty_PNG28.png" alt="" />
-            <p className="speechText" onClick={setNewLine}>{text}</p>
+            <img src={dialogue[turn].image} alt="" />
+            <div className="speechText" onClick={handleText}>
+                <p><b>{dialogue[turn].name}</b></p>
+                <p>{text}</p>
+            </div>
             <button>Close</button>
         </div>
     )
